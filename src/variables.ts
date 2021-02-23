e86cc83fed47886ec1028975b313f81dec4ae2f0
Olga Milczek <olga.milczek@movecloser.pl>
2021-02-23 18:05:56 +0100

    Add file with variables

---
+++ src/variables.ts
@@ -0,0 +1,14 @@
+export const WIDTH = window.innerWidth - 10;
+export const HEIGHT = window.innerHeight - 10;
+export const BALL_SIZE = 30;
+export const RACKET_SIZE = 180;
+export const RACKET_WEIGHT = 10;
+export const INITIAL_VELOCITY = 3;
+export const RACKET_OFFSET = 100;
+
+export const TOLERANCE = 10;
+
+export const OBSTACLES = [
+    'wall',
+    'racket'
+];
